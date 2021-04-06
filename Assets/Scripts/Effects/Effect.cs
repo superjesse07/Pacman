e58c812fa8ec5ac6d655edb81cdd32d7c412ef14
Jesse Rosendahl <jesse@jessar.nl>
2021-04-06 14:00:54 +0200

    Added Effect

---
+++ Assets/Scripts/Effects/Effect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Effect : MonoBehaviour
+{
+    [NonSerialized]public string name;
+    [NonSerialized]public string description;
+    [NonSerialized]public float duration;
+    
+    public bool isActive;
+    public abstract void Activate();
+    public abstract void DeActivate();
+}
---
+++ Assets/Scripts/Effects/UpsideDown.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpsideDown : Effect
+{
+
+    public Camera camera;
+    public override void Activate()
+    {
+        isActive = true;
+        camera.transform.Rotate(0,0,180);
+    }
+
+    public override void DeActivate()
+    {
+        camera.transform.Rotate(0,0,180);
+        isActive = false;
+    }
+}
